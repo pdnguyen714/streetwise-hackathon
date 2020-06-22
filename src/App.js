614e5f3286ff0380b7fd261105dddff595e5ec8f
Peter <pdnguyen714@gmail.com>
2020-06-22 15:31:41 -0700

    configured google maps API with axios and foursquare api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import axios from 'axios'
 
 class App extends Component() {
 
   componentDidMount() {
-    this.renderMap()
+    this.getVenues()
   }
 
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCkqzhknCSc2mV6evpPKPtLzNZVs-OEs4M&callback=initMap")
     window.init = this.initMap
   }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "XEWEQFRNIC4IXCGG2CFKRLC2GMUKLW001NWL42TSS1LEDVJ4",
+      client_secret: "IBYOXDNUOQ5Z5HSKZUIZ03RXDNWFKNXCPU33B40PXPH1R4OM",
+      query: "food",
+      near: "Orange",
+      v: "20182507"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items
+      }, this.renderMap())
+    })
+    .catch(error => {
+      console.log("ERROR!! " + error)
+    })
 
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById("map"), {
@@ -39,3 +60,6 @@ function loadScript(url) {
 }
 
 export default App;
+
+
+loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCkqzhknCSc2mV6evpPKPtLzNZVs-OEs4M&callback=initMap")
