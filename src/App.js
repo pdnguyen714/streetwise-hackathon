60f3c98e7e9ca809a6bcdbf742589b0bb989946d
briahnayee <briahnayee@gmail.com>
2020-06-23 12:10:36 -0700

    reorganized, added react components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Map } from './components/Map/Map.js'
+import { SideBar } from './components/SideBar/SideBar';
+
 
 function App() {
   return (
     <div className="App">
-
+      <SideBar />
+      <Map />
     </div>
   );
 }
