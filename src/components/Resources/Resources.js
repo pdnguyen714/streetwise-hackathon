f53f5640401f727617ef49770bd64eb47f9589b8
Peter <pdnguyen714@gmail.com>
2020-06-24 10:00:28 -0700

    refactored map.js and began resources.js template and styling

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,12 +1,83 @@
 import React from 'react';
 import './Resources.css'
+// import { Nav } from '../Nav/Nav';
 
 export const Resources = (props) => {
 
     return (
-        <div className="resources">
-
-        </div>
+        <>
+            {/* <Nav /> */}
+            <div className="resources">
+                <div className="resource-row">
+                    <div className="resource-content">
+                        <h1 className="resource-header">Protest Information</h1>
+                        <p className="resource-p">Placeholder Text</p>
+                        <div className="resource-links">
+                            <div className="resource-link1">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">Know your Rights</a><br />
+                            </div>
+                            <div className="resource-link2">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">How to organize a protest</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="resource-img">
+                        <img src="/protest.jpg" alt="protest" height="500" width="500" />
+                    </div>
+                </div>
+                <div className="resource-row-reverse">
+                    <div className="resource-content">
+                        <h1 className="resource-header">Protest Information</h1>
+                        <p className="resource-p">Placeholder Text</p>
+                        <div className="resource-links">
+                            <div className="resource-link1">
+                                    <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">Know your Rights</a><br />
+                            </div>
+                            <div className="resource-link2">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">How to organize a protest</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="resource-img">
+                        <img src="/protest.jpg" alt="protest" height="500" width="500" />
+                    </div>
+                </div>
+                <div className="resource-row">
+                    <div className="resource-content">
+                        <h1 className="resource-header">Protest Information</h1>
+                        <p className="resource-p">Placeholder Text</p>
+                        <div className="resource-links">
+                            <div className="resource-link1">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">Know your Rights</a><br />
+                            </div>
+                            <div className="resource-link2">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">How to organize a protest</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="resource-img">
+                        <img src="/protest.jpg" alt="protest" height="500" width="500" />
+                    </div>
+                </div>
+                <div className="resource-row-reverse">
+                    <div className="resource-content">
+                        <h1 className="resource-header">Protest Information</h1>
+                        <p className="resource-p">Placeholder Text</p>
+                        <div className="resource-links">
+                            <div className="resource-link1">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">Know your Rights</a><br />
+                            </div>
+                            <div className="resource-link2">
+                                <a href="https://www.figma.com/file/0paDALoKy0GhyvKtJtc8TI/Project-Streetwise-v2?node-id=0%3A1">How to organize a protest</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="resource-img">
+                        <img src="/protest.jpg" alt="protest" height="500" width="500" />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
